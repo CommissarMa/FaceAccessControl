c2451b5b771a0cb6de5d8a748b969d147e400155
CommissarMa <960901625@qq.com>
2017-04-19 19:53:40 +0800

    First Initiate

---
+++ app/src/main/java/cn/edu/ecust/faceaccesscontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.ecust.faceaccesscontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
